734f2e6536afe4bb289e7f0d3a3efb51f0936bed
Galen Guyer <galen@galenguyer.com>
2021-02-27 02:26:55 -0500

    Add very basic Flask JSON API

---
+++ backend/api/__init__.py
@@ -0,0 +1,19 @@
+""" A small flask Hello World """
+
+import os
+
+from flask import Flask, jsonify
+
+APP = Flask(__name__)
+
+# Load file based configuration overrides if present
+if os.path.exists(os.path.join(os.getcwd(), 'config.py')):
+    APP.config.from_pyfile(os.path.join(os.getcwd(), 'config.py'))
+else:
+    APP.config.from_pyfile(os.path.join(os.getcwd(), 'config.env.py'))
+
+APP.secret_key = APP.config['SECRET_KEY']
+
+@APP.route('/')
+def _index():
+    return jsonify(status=200, response='OK')
